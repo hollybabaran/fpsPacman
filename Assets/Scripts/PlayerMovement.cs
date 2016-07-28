cf1f7a1c8713f2fc887f91740f5eb857133bee88
HOLLY BABARAN <hbabaran@uwaterloo.ca>
2016-07-28 09:24:12 -0400

    added imported fps controls

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,7 +4,10 @@ using System.Collections;
 public class PlayerMovement : MonoBehaviour {
 
     private Rigidbody rb;
-    public float speed;
+    //public vars are modified on-screen in unity
+    public float forwardSpeed; 
+    public float strafeSpeed;
+    public float collisionCheckDistance;
 
     // Use this for initialization
     void Start () {
@@ -15,17 +18,26 @@ public class PlayerMovement : MonoBehaviour {
 	void FixedUpdate () {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-        rb.AddForce(movement * speed);
+        //rb.moveRotation on camera
+        Vector3 movement = new Vector3(moveHorizontal*strafeSpeed, 0.0f, moveVertical*forwardSpeed);
+        RaycastHit hit;
+        if (!rb.SweepTest(movement, out hit, collisionCheckDistance)){
+            rb.MovePosition(rb.position + movement);
+        }
+        else{
+            rb.MovePosition(hit.point);
+        }
+        
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        //if (other.gameObject.CompareTag("Wall"))
+        //{
+        //    rb.MovePosition(rb.position);
+        //}
     }
 }
 
-
-//    void OnTriggerEnter(Collider other)
-//    {
-//        if (other.gameObject.CompareTag("Pickup"))
-//        {
-//            other.gameObject.SetActive(false);
-//        }
-//    }
-//}
+//for pickups: other.gameObject.SetActive(false);
