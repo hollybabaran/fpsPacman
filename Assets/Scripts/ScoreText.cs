6a15b78e5dcbeab0277828fd65ff212f3830254b
HOLLY BABARAN <hbabaran@uwaterloo.ca>
2016-08-06 18:13:59 -0400

    trying to make score appear

---
+++ Assets/Scripts/ScoreText.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreText : MonoBehaviour {
+
+    // Use this for initialization
+    private GUIText scoreText; 
+
+	void Start () {
+        scoreText = GetComponent<GUIText>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        scoreText.text = "candies remaining: ";
+	}
+}
