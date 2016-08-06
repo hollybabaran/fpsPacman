1b17bbeefe8b832eb4be801e878bc1297b3ccb8e
HOLLY BABARAN <hbabaran@uwaterloo.ca>
2016-08-06 15:52:19 -0400

    fixed bobbing yay, placed many pickups

--- Assets/Scripts/Bob.cs
+++ Assets/Scripts/Bob.cs
@@ -1,25 +1,39 @@
 ﻿using UnityEngine;
-using System.Collections;
 using UnityStandardAssets.Utility;
 
 public class Bob : MonoBehaviour
 {
     private Rigidbody rb;
-    public LerpControlledBob bob = new LerpControlledBob();
-
+    public float amplitude;
+    public float duration;
+    
+    private bool direction; //true for going up 
+    private float time; 
     // Update is called once per frame
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        StartCoroutine(bob.DoBobCycle());
+        direction = true;
+        time = 0f; 
     }
 
     void FixedUpdate()
     {
+        float t = Time.deltaTime;
+        time += t;
+        if (time > duration){
+            direction = !direction;
+            time = 0; 
+        }
         Vector3 pos = rb.position;
-        pos.y += bob.Offset();
-        print(pos.y);
+        pos.y += getOffset(t);
         rb.MovePosition(pos);
-        StartCoroutine(bob.DoBobCycle());
+    }
+
+    //returns a positive offset if going up, negative if going down 
+    private float getOffset(float t){
+        float offset = (amplitude / duration) * t;
+        offset = direction ? offset : -1 * offset; 
+        return offset; 
     }
 }
